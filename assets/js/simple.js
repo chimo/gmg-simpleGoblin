0b32bbc6e97b3a4172ac5b84309befd09926ffe3
Chimo <chimo@chromic.org>
2015-05-30 22:54:50 -0400

    Initial commit

---
+++ assets/js/simple.js
@@ -0,0 +1,9 @@
+( function( $ ) {
+    /* exif dropdown */
+    $( "#exif_additional_info_button" ).on( "click", function() {
+        $( "#exif_additional_info" ).slideToggle( "slow" );
+    } );
+
+    /* TODO - maps */
+}( jQuery ) );
+
